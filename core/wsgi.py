7d293f37a3bdf71b3f5dd19415f56969f639d15a
Asilbek <a.xalilov@uic.group>
2023-03-27 06:04:54 +0000

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI core for core project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import environ
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-environ.Env.read_env(".env")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
